f4f367bbd707b08763109ec4ff7cdd93065a12a0
Satish Timalsina <timalsinasatish430@gmail.com>
2024-11-20 09:09:49 +0545

    first commit

---
+++ CsharpOperator.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace project1
+{
+    internal class Csharpoperator
+    {
+        //public void run()
+        //{
+        //    arithop();
+        //}
+        // void arithop()
+        //{
+        //    int a = 1;
+        //    int b= 2;
+        //    int result = a + b;
+        //    Console.WriteLine(result);
+        //}
+        public void run()
+        {
+            greater();
+        }
+        void greater() {
+            Console.WriteLine("Enter a number:");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            string string2 = Console.ReadLine();
+            int number1 = int.Parse(string1);
+            int number2 = int.Parse(string2);
+            int result=(number1>number2 ? number1 : number2);
+            Console.WriteLine("the greater value is "+result);
+            
+            
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using project1;
+Csharpoperator ram = new Csharpoperator();
+ram.run();
+Class1 shyam  =new Class1();
+shyam.bun();
+
